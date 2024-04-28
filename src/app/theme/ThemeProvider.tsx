313e8f298a56c423c56d546eae49277b7d652dfb
Oskar <oskar.stud@gmail.com>
2024-04-29 00:07:12 +0300

    Добавлен переключатель темы и доп цвет для темной темы

--- src/app/theme/ThemeProvider.tsx
+++ src/app/theme/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { Theme } from './type';
 import * as styles from './ThemeProvider.module.scss';
 
@@ -15,9 +15,26 @@ const ThemeContext = createContext<ThemeContextProps>({});
 
 export const useThemeContext = (): ThemeContextProps => useContext(ThemeContext);
 
+const getTheme = (): Theme => {
+  const theme = `${window?.localStorage?.getItem('theme')}`;
+  if (Object.values<string>(Theme).includes(theme)) return theme as Theme;
+
+  const userMedia = window.matchMedia('(prefers-color-scheme: light)');
+  if (userMedia.matches) return Theme.light;
+
+  return Theme.dark;
+};
+
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState(getTheme);
+  console.log('@@@', theme);
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
-    <ThemeContext.Provider value={{ theme: Theme.light }}>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className={styles}>{children}</div>
     </ThemeContext.Provider>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import * as styles from './Header.module.scss';
 import { Logo } from '../Logo';
+import { ThemeSwitcher } from './Parts/ThemeSwitcher';
 
 export const Header: React.FC = (): React.ReactElement => {
   return (
     <div className={styles.header}>
       <Logo />
+      <ThemeSwitcher />
     </div>
   );
 };
